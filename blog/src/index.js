3b62fb0b439bb29cc1d73bd98ee52017c305d5c9
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-09 14:07:09 +0700

    Bai 22

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,22 +5,27 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
