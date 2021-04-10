49589e904d42a4aeb12d1075e1fe4a7c5bb5a20c
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-04-10 12:53:10 +0700

    Bai 24

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,11 +2,17 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
-
+// Use statis folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -27,11 +33,11 @@ app.engine(
   }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
